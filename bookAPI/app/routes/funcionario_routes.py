f7ebfe59aa2b221fc8d7bb560b0af1d7896f50cb
cauanwarlly <148079161+cauanwarlly@users.noreply.github.com>
2025-06-30 17:16:18 -0300

    Add files via upload

--- bookAPI/app/routes/funcionario_routes.py
+++ bookAPI/app/routes/funcionario_routes.py
@@ -3,12 +3,108 @@ from app.controllers import funcionario_controller
 
 funcionario_bp = Blueprint("funcionarios", __name__)
 
+@funcionario_bp.route("/funcionarios", methods=["POST"])
+def add_funcionario():
+    """
+    Cria um novo funcionário
+    ---
+    tags:
+      - Funcionários
+    parameters:
+      - in: body
+        name: corpo
+        schema:
+          type: object
+          properties:
+            nome:
+              type: string
+              example: João Silva
+            idade:
+              type: integer
+              example: 30
+            cargo:
+              type: string
+              example: Analista
+    responses:
+      201:
+        description: Funcionário criado com sucesso
+    """
+    return funcionario_controller.add_funcionario()
 
+@funcionario_bp.route("/funcionarios", methods=["GET"])
+def get_funcionarios():
+    """
+    Lista todos os funcionários
+    ---
+    tags:
+      - Funcionários
+    responses:
+      200:
+        description: Lista de funcionários
+    """
+    return funcionario_controller.get_funcionarios()
 
+@funcionario_bp.route("/funcionarios/<int:funcionario_id>", methods=["GET"])
+def get_funcionario(funcionario_id):
+    """
+    Retorna um funcionário específico pelo ID
+    ---
+    tags:
+      - Funcionários
+    parameters:
+      - in: path
+        name: funcionario_id
+        type: integer
+        required: true
+    responses:
+      200:
+        description: Funcionário encontrado
+    """
+    return funcionario_controller.get_funcionario(funcionario_id)
 
+@funcionario_bp.route("/funcionarios/<int:funcionario_id>", methods=["PUT"])
+def update_funcionario(funcionario_id):
+    """
+    Atualiza um funcionário
+    ---
+    tags:
+      - Funcionários
+    parameters:
+      - in: path
+        name: funcionario_id
+        type: integer
+        required: true
+      - in: body
+        name: corpo
+        schema:
+          type: object
+          properties:
+            nome:
+              type: string
+            idade:
+              type: integer
+            cargo:
+              type: string
+    responses:
+      200:
+        description: Funcionário atualizado com sucesso
+    """
+    return funcionario_controller.update_funcionario(funcionario_id)
 
-funcionario_bp.post("/funcionarios")(funcionario_controller.add_funcionario)
-funcionario_bp.get("/funcionarios")(funcionario_controller.get_funcionarios)
-funcionario_bp.get("/funcionarios/<int:funcionario_id>")(funcionario_controller.get_funcionario)
-funcionario_bp.put("/funcionarios/<int:funcionario_id>")(funcionario_controller.update_funcionario)
-funcionario_bp.delete("/funcionarios/<int:funcionario_id>")(funcionario_controller.delete_funcionario)
+@funcionario_bp.route("/funcionarios/<int:funcionario_id>", methods=["DELETE"])
+def delete_funcionario(funcionario_id):
+    """
+    Deleta um funcionário pelo ID
+    ---
+    tags:
+      - Funcionários
+    parameters:
+      - in: path
+        name: funcionario_id
+        type: integer
+        required: true
+    responses:
+      200:
+        description: Funcionario deletado com sucesso
+    """
+    return funcionario_controller.delete_funcionario(funcionario_id)
--- bookAPI/run.py
+++ bookAPI/run.py
@@ -1,5 +1,4 @@
 from app import create_app
-from main import app
 
 
 app = create_app()
